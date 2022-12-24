b1b20dfcf480d59eaca9be831bb89b3103d52cfb
Mitch <70273353+MVG47@users.noreply.github.com>
2022-12-24 02:15:32 +0000

    Co-authored-by: hossam sulleman <hossam1231@users.noreply.github.com>

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,25 +1,18 @@
 import styled from 'styled-components'
-
+import NewsList from 'src/components/Newsletter Section/NewsList'
 import ProductsCell from 'src/components/ProductsCell'
 
 const HomePage = () => {
   return (
     <Wrapper>
       <article>
-        <h2>Super Tokens</h2>
+        <img className="w-24 h-24" src='https://cdn.discordapp.com/attachments/1015719580385153110/1056017368104570960/IMG_1081.JPG'/>
+        <h2> </h2>
+        <NewsList/>
         <Description>
           These are single-use tokens. Great for emergencies and gifts.
         </Description>
         <ProductsCell />
-      </article>
-
-      <article>
-        <h2>Super Subs</h2>
-        <Description>
-          Monthly subscriptions to superpowers. Great for the career
-          supervillain, hero or parent.
-        </Description>
-        <ProductsCell type={'recurring'} />
       </article>
     </Wrapper>
   )
